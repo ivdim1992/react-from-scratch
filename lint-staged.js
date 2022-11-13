59eb6f63572ea688e67a097a30b4ad23016b1e92
ivdim <ivdim1992@gmail.com>
2022-11-13 22:37:37 +0200

    husky added

--- lint-staged.js
+++ lint-staged.js
@@ -1,5 +1,6 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-    '*.{js,jsx,ts,tsx}': [
+    '*.{jsx}': [
         'eslint --max-warnings=0',
         'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests',
         () => 'tsc-files --noEmit',
